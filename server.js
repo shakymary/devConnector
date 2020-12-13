2702aa0076789adb0ac3f54e1e9eef1570fcd064
Shakila Marando <marandoshakila1@gmail.com>
2020-12-12 18:55:34 -0800

    added routers

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect Database
+
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running"));
+
+//Define Routes
+app.use("/api/users", require("./config/routes/api/users"));
+app.use("/api/auth", require("./config/routes/api/auth"));
+app.use("/api/profile", require("./config/routes/api/profile"));
+app.use("/api/posts", require("./config/routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
